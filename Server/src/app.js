92a24b89a5e02f23c2893b031dfab01ae6af74f3
Nerea <NereaMansillaBarreiro@gmail.com>
2022-11-10 03:31:42 -0300

    changes again

--- Server/src/app.js
+++ Server/src/app.js
@@ -4,21 +4,25 @@ import taskRoutes from './routes/task.routes.js'
 import userRoutes from './routes/user.routes.js' */
 import router from './routes/index.js'
 import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
+app.use(cors({
+  origin: "*"
+}))
 app.use(express.json())
 /* app.use(urlencoded()) */
-app.use(router)
-app.use(cors())
+app.use('/', router)
+app.use(morgan('dev'))
 
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type,  Authorization, Accept")
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
       next();
  
-});
+}); */
 
 /* app.use(routesProject)
 app.use(taskRoutes)
